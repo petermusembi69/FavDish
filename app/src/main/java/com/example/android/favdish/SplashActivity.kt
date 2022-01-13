703e41f7c378ba197da32d2d54337fecb811d947
petermusembi69 <nyongesamusembi@gmail.com>
2022-01-13 12:33:44 +0300

    Hide navigation bar

--- app/src/main/java/com/example/android/favdish/SplashActivity.kt
+++ app/src/main/java/com/example/android/favdish/SplashActivity.kt
@@ -2,15 +2,16 @@ package com.example.android.favdish
 
 import android.content.Intent
 import android.os.Build
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
+import android.view.View
 import android.view.WindowInsets
+import android.view.WindowInsetsController
 import android.view.WindowManager
 import android.view.animation.Animation
-import android.view.animation.Animation.AnimationListener
 import android.view.animation.AnimationUtils
+import androidx.appcompat.app.AppCompatActivity
 import com.example.android.favdish.databinding.ActivitySplashBinding
 
 class SplashActivity : AppCompatActivity() {
@@ -20,27 +21,38 @@ class SplashActivity : AppCompatActivity() {
         val splashBinding: ActivitySplashBinding = ActivitySplashBinding.inflate(layoutInflater)
         setContentView(splashBinding.root)
 
-        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
-            window.insetsController?.hide(WindowInsets.Type.statusBars())
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+            window.insetsController?.let {
+                it.hide(WindowInsets.Type.systemBars())
+            }
         } else {
             @Suppress("DEPRECATION")
-            window.setFlags(
-                WindowManager.LayoutParams.FLAG_FULLSCREEN,
-                WindowManager.LayoutParams.FLAG_FULLSCREEN
-            )
+            window.decorView.systemUiVisibility = (
+                    View.SYSTEM_UI_FLAG_IMMERSIVE
+                            or View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                            or View.SYSTEM_UI_FLAG_FULLSCREEN
+                            or View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                            or View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN)
+            @Suppress("DEPRECATION")
+            window.addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_NAVIGATION)
         }
 
-        val splashAnimation = AnimationUtils.loadAnimation(this,R.anim.splash_screen_animation)
+
+    val splashAnimation = AnimationUtils.loadAnimation(this, R.anim.splash_screen_animation)
         splashBinding.appName.animation = splashAnimation
 
-        splashAnimation.setAnimationListener(object: Animation.AnimationListener {
+        splashAnimation.setAnimationListener(object : Animation.AnimationListener {
             override fun onAnimationStart(animation: Animation?) {
             }
 
             override fun onAnimationEnd(animation: Animation?) {
                 Handler(Looper.getMainLooper()).postDelayed({
-                    startActivity(Intent(this@SplashActivity,
-                        MainActivity::class.java))
+                    startActivity(
+                        Intent(
+                            this@SplashActivity,
+                            MainActivity::class.java
+                        )
+                    )
                     finish()
                 }, 1000)
             }
