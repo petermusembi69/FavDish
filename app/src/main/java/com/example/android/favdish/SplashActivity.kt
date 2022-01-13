d7078295ba2813eb562a3b6090a34bc7f9061c48
petermusembi69 <nyongesamusembi@gmail.com>
2022-01-13 11:59:06 +0300

    Add animations alpha and transition

--- app/src/main/java/com/example/android/favdish/SplashActivity.kt
+++ app/src/main/java/com/example/android/favdish/SplashActivity.kt
@@ -1,10 +1,16 @@
 package com.example.android.favdish
 
+import android.content.Intent
 import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import android.view.WindowInsets
 import android.view.WindowManager
+import android.view.animation.Animation
+import android.view.animation.Animation.AnimationListener
+import android.view.animation.AnimationUtils
 import com.example.android.favdish.databinding.ActivitySplashBinding
 
 class SplashActivity : AppCompatActivity() {
@@ -23,5 +29,26 @@ class SplashActivity : AppCompatActivity() {
                 WindowManager.LayoutParams.FLAG_FULLSCREEN
             )
         }
+
+        val splashAnimation = AnimationUtils.loadAnimation(this,R.anim.splash_screen_animation)
+        splashBinding.appName.animation = splashAnimation
+
+        splashAnimation.setAnimationListener(object: Animation.AnimationListener {
+            override fun onAnimationStart(animation: Animation?) {
+            }
+
+            override fun onAnimationEnd(animation: Animation?) {
+                Handler(Looper.getMainLooper()).postDelayed({
+                    startActivity(Intent(this@SplashActivity,
+                        MainActivity::class.java))
+                    finish()
+                }, 1000)
+            }
+
+            override fun onAnimationRepeat(animation: Animation?) {
+            }
+
+        })
+
     }
 }
